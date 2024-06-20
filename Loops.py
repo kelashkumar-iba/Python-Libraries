bc3e83b466aad2c516565db9468577be4f242a7d
Kelash Kumar <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-20 18:20:28 +0500

    Add Loops.py

--- Loops.py
+++ Loops.py
@@ -1,4 +1,8 @@
-age = 18
-while(age <= 10):
-    print(age)
-    age = age + 1
+# Iterating over a list
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    print(num)
+
+# Iterating over a string
+for char in "Python":
+    print(char)
