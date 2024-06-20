4b888104da2041e9a179736acc0897fbd09be891
Kelash Kumar <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-20 18:46:55 +0500

    Add Loops.py

--- Loops.py
+++ Loops.py
@@ -7,7 +7,6 @@ for num in numbers:
 for char in "Python":
     print(char)
 
-
 # Using break
 for num in range(10):
     if num == 5:
@@ -20,9 +19,31 @@ for num in range(10):
         continue
     print(num)
 
+# Nested for loop
+for i in range(3):
+    for j in range(2):
+        print(f'i = {i}, j = {j}')
 
 # Printing numbers from 1 to 5 using the (while loop)
 count = 1
 while count <= 5:
     print(count)
     count += 1
+
+# Practical example
+# Find prime numbers within a range
+start = 10
+end = 50
+
+for num in range(start, end + 1):
+    # Prime numbers are greater than 1
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
+
+
+
+
