dc7d5eddb8f3b5b21fe54764f56f8132c01115c9
Kelash Kumar <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-20 18:29:27 +0500

    Add Loops.py

--- Loops.py
+++ Loops.py
@@ -6,3 +6,23 @@ for num in numbers:
 # Iterating over a string
 for char in "Python":
     print(char)
+
+
+# Using break
+for num in range(10):
+    if num == 5:
+        break
+    print(num)
+
+# Using continue
+for num in range(10):
+    if num % 2 == 0:
+        continue
+    print(num)
+
+
+# Printing numbers from 1 to 5 using the (while loop)
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
