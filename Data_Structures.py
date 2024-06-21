b422b21887ac32cbd342f129597a7b13457c2a91
Kelash Kumar <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-21 17:23:29 +0500

    List, Tuple, Set, Dictionary

---
+++ Data_Structures.py
@@ -0,0 +1,22 @@
+#List
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+
+# Modifying a list
+my_list.append(6)
+my_list[2] = 10
+
+# Accessing elements
+print(my_list[0])  # Output: 1
+print(my_list)     # Output: [1, 2, 10, 4, 5, 6]
+
+#Practical Example
+# List of names
+names = ['Kelash', 'Zeesan', 'Saleh', 'Danish']
+
+# Adding a new name
+names.append('Jai')
+
+# Printing all names
+for name in names:
+    print(name)
