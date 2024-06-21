14be03fdaa11f7a1f2ce21197b7deeb06ef89a88
Kelash Kumar <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-21 17:34:48 +0500

    Lists, Sets and Dictionaries

--- Data_Structures.py
+++ Data_Structures.py
@@ -1,4 +1,4 @@
-#List
+#LIST:
 # Creating a list
 my_list = [1, 2, 3, 4, 5]
 
@@ -20,3 +20,34 @@ names.append('Jai')
 # Printing all names
 for name in names:
     print(name)
+
+#SET:
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+
+# Adding elements
+my_set.add(6)
+
+# Removing elements
+my_set.remove(3)
+
+# Set operations
+other_set = {4, 5, 6, 7}
+union_set = my_set.union(other_set)
+print(union_set)  # Output: {1, 2, 4, 5, 6, 7}
+
+#Practical Example.
+# Tags for a blog post
+tags = {'python', 'programming', 'tutorial'}
+
+# Adding a new tag
+tags.add('beginner')
+
+# Checking if a tag exists
+if 'python' in tags:
+    print("Python tag found!")
+
+# Removing a tag
+tags.remove('tutorial')
+print(tags)  # Output: {'python', 'programming', 'beginner'}
+
