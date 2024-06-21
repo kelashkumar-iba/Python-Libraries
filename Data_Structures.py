39a058aeea72e2c84367d60448c48f7dfd8cec3e
Kelash Kumar <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-21 17:40:56 +0500

    Lists, Sets and Dictionaries

--- Data_Structures.py
+++ Data_Structures.py
@@ -1,4 +1,4 @@
-#LIST:
+# LIST:
 # Creating a list
 my_list = [1, 2, 3, 4, 5]
 
@@ -10,7 +10,7 @@ my_list[2] = 10
 print(my_list[0])  # Output: 1
 print(my_list)     # Output: [1, 2, 10, 4, 5, 6]
 
-#Practical Example
+# Practical Example
 # List of names
 names = ['Kelash', 'Zeesan', 'Saleh', 'Danish']
 
@@ -21,7 +21,7 @@ names.append('Jai')
 for name in names:
     print(name)
 
-#SET:
+# SET:
 # Creating a set
 my_set = {1, 2, 3, 4, 5}
 
@@ -36,7 +36,7 @@ other_set = {4, 5, 6, 7}
 union_set = my_set.union(other_set)
 print(union_set)  # Output: {1, 2, 4, 5, 6, 7}
 
-#Practical Example.
+# Practical Example.
 # Tags for a blog post
 tags = {'python', 'programming', 'tutorial'}
 
@@ -51,3 +51,34 @@ if 'python' in tags:
 tags.remove('tutorial')
 print(tags)  # Output: {'python', 'programming', 'beginner'}
 
+# DICTIONARY:
+# Creating a dictionary
+my_dict = {'name': 'Kelash', 'age': 20, 'city': 'Sukkur'}
+
+# Accessing values
+print(my_dict['name'])  # Output: 'Kelash'
+
+# Modifying values
+my_dict['age'] = 21
+
+# Adding new key-value pairs
+my_dict['job'] = 'Engineer'
+
+# Dictionary operations
+del my_dict['city']
+print(my_dict)  # Output: {'name': 'Kelash', 'age': 21, 'job': 'Engineer'}
+
+# Practical Example
+# Information about a person
+person = {
+    'Name': 'Ali',
+    'Age': 23,
+    'City': 'Sukkur'
+}
+
+# Adding a new attribute
+person['Occupation'] = 'Software Developer'
+
+# Printing all attributes
+for key, value in person.items():
+    print(f"{key}: {value}")
