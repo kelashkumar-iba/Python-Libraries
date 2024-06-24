1278cd906cbdf9d7cffa77307746457649f2f5f5
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-24 11:31:28 +0500

    NumPy

---
+++ NumPy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# From a list
+arr1 = np.array([1, 2, 3, 4, 5])
+print(arr1)  # Output: [1 2 3 4 5]
+
+# From a tuple
+arr2 = np.array((1, 2, 3, 4, 5))
+print(arr2)  # Output: [1 2 3 4 5]
