4e77dedf8c6e2e9a142bc66d14ead59cade1a842
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-25 12:06:19 +0500

    Pandas DataFrames

--- Pandas.py
+++ Pandas.py
@@ -4,6 +4,7 @@ import pandas as pd
 data = {'Name': ['Love', 'Haresh', 'Kelash', 'Zeeshan'],
         'Age': [20, 18, 21, 23],
         'City': ['Naukot', 'Mithi', 'Sukkur', 'Karachi']}
+
 df = pd.DataFrame(data)
 
 print(df)
@@ -22,3 +23,24 @@ print(df.info())
 
 # Display summary statistics of numerical columns
 print(df.describe())
+
+# Display the shape of the DataFrame
+print(df.shape)
+
+# Select a single column
+print(df['avg_temp_c'])
+
+# Select multiple columns
+print(df[['city', 'avg_temp_c']])
+
+# Select rows by index
+print(df.loc[0])  # First row
+print(df.loc[1:5])  # First five rows
+
+# Select rows and columns by label
+print(df.loc[1:5, ['date', 'avg_temp_c']])
+
+# Select rows and columns by position
+print(df.iloc[0])  # First row
+print(df.iloc[1:5, [0, 3]])  # First five rows, first and fourth columns
+
