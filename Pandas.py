c2cdfba7f38bcad8e42752e1adbe0d7f59aa48c7
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-25 11:46:04 +0500

    Pandas DataFrames

--- Pandas.py
+++ Pandas.py
@@ -7,3 +7,18 @@ data = {'Name': ['Love', 'Haresh', 'Kelash', 'Zeeshan'],
 df = pd.DataFrame(data)
 
 print(df)
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv("C:\\Users\Kelash Kumar\\Downloads\\temperatures.csv")
+
+# Display the first few rows of the DataFrame
+print(df.head())
+
+# Display the last few rows of the DataFrame
+print(df.tail())
+
+# Display basic information about the DataFrame
+print(df.info())
+
+# Display summary statistics of numerical columns
+print(df.describe())
